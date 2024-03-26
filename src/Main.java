a93d9a3293f3ee8fea756c5ac8727af4bb99ba09
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-26 06:44:57 -0300

    Bucle while y bucle do while

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Character char01 = 'A';
+
+        while(char01 <= 'Z'){
+            System.out.println(char01);
+            char01++;
+        }
+        System.out.println("-------------------------------------------");
+        int num = 2;
+        int exponente = 0;
+        while (exponente <= 9) {
+            System.out.println("2 elevado a la "+exponente+" es igual a: " +Math.pow(num,exponente));
+            exponente++;
+        }
+        System.out.println("-------------------------------------------");
+
+        String letraOculta = "C";
+        String letraIngresada = "null";
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Adivine la letra\n");
+            letraIngresada = input.next();
+        } while (!letraIngresada.equalsIgnoreCase(letraOculta));
+        System.out.println("Genial! Adivinó la letra y esta era la: "+letraOculta );
+
     }
 }
